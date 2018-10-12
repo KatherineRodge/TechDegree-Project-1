f37c8888f8d79738d438ccbd27e5d4a1ba6cd7e9
KatherineRodge <rodgek@mail.gvsu.edu>
2018-10-11 21:49:30 -0400

    Creating a Random Number

--- js/script.js
+++ js/script.js
@@ -1,13 +1,84 @@
-// FSJS - Random Quote Generator
-
-// Create the array of quote objects and name it quotes
-
-
-
-// Create the getRandomQuuote function and name it getRandomQuote
+//variables used
+var randomNumber;
+var randomQuote;
 
 
+//Array of Quotes, Authors, Citations, Years, and Categories as as applicable
 
+var quotes = [
+ {quote: "We can only learn to love by loving.",
+  speaker: "Iris Murdoch",
+  source: "O Magazine",
+  year: "February 2004",
+  category: "Thought Provoking"
+},
+
+ {quote: "We touch other peoples lives simply by existing.",
+  speaker: "JK Rowling",
+  source: "Harvard Commencement Address",
+  year: "2008",
+  category: "Thought Provoking"
+},
+
+ {quote: "To be proud of learning is the greatest ignorance.",
+  speaker: "Jeremy Taylor",
+  category: "Thought Provoking"
+ },
+
+ {quote: "Everything that irritates us about others can lead us to an understanding of ourselves.",
+  speaker: "Carl Jung",
+  category: "Thought Provoking"
+ },
+
+ {quote: "Eat a live toad the first thing in the morning and nothing worse will happen to you the rest of the day.",
+  speaker: "Unknown",
+  category: "Humor"
+ },
+
+ {quote: "Life was so much easier when I was doing pizza slime galaxy videos.",
+  speaker: "Shane Dawson",
+  source: "Twitter",
+  year: "2018",
+  category: "Humor"
+},
+
+ {quote:"You are not weird. You are not stupid. You do not need to try harder. You are not a failed version of normal. You are different, you are beautiful, and you are not alone.",
+  speaker: "Jessica McCabe",
+  source: "Failing at Normal: An ADHD Success Story",
+  year: "2017",
+  category: "Inspirational"
+},
+
+ {quote:"Even if you're on the right track, you'll get run over if you just sit there.",
+  speaker: "Will Rodgers",
+  category: "Humor"
+ },
+
+ {quote: "A man is a success if he gets up in the morning and gets to bed at night, and in between he does what he wants to do.",
+  speaker: "Bob Dylan",
+  category: "Thought Provoking"
+ },
+
+ {quote: "I like to play blackjack. I'm not addicted to gambling, I'm addicted to sitting in a semi-circle.",
+  speaker: "Mitch Hedgeburg",
+  category: "Humor"
+ },
+];
+
+
+// get random quote randomly pulls a quote & information from the quotes array
+
+function getRandomQuote(array) {
+
+//get a random number to pull an object from the array
+//Between 0 and array.length
+var betweenNumbersUpper = array.length;
+randomNumber = Math.floor(Math.random() * betweenNumbersUpper);
+return randomNumber;
+
+};
+
+console.log(getRandomQuote(quotes));
 // Create the printQuote funtion and name it printQuote
 
 
